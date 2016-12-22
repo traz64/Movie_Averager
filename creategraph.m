1b7265a1d89c93d4d65b80a5b8a21400d9762a66
Traz Daczkowski <trazdacz1@gmail.com>
2016-12-22 03:41:42 -0500

    added thresholding

--- creategraph.m
+++ creategraph.m
@@ -1,4 +1,4 @@
-function creategraph(file,ar,fpp,mode)
+function creategraph(file,ar,fpp,mode,thresh)
 video=VideoReader(file);
 i=1;
 xdim=floor(video.Duration*video.FrameRate/fpp);
@@ -11,24 +11,22 @@ while(i<=xdim)
     for x=1:fpp
         inputbuffer(:,:,:,x)=readFrame(video);
     end
-    
+    coloronlastdim=shiftdim(inputbuffer,-1);
+    colorvec=reshape(coloronlastdim,[],3);
+    colorvec(sum(colorvec)<=thresh,:)=[];
         switch mode
             case 0 %mean
-                output(i,1,:)=mean(mean(mean(intputbuffer,4),2),1);
+                output(i,1,:)=mean(colorvec,1);
                 
             case 1 %median
-                coloronlastdim=shiftdim(inputbuffer,-1);
-                
-                output(i,1,:)=median(reshape(coloronlastdim,[],3),1);
-            case 2 %mode non black pixels
-                coloronlastdim=shiftdim(inputbuffer,-1);
-                colorvec=reshape(coloronlastdim,[],3);
-                output(i,1,:)=mode(colorvec(sum(colorvec)>0,:),1);
+                output(i,1,:)=median(colorvec,1);
+            case 2 %mode
+                output(i,1,:)=mode(colorvec,1);
         end
     
     
 end
-
+output=output/255;
 output=repmat(output,1,round(xdim/ar),1);
 figure
 imshow(output)
