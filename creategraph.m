53b9075d184bfa4d1f3b7c0988bab32d77dd5120
Traz Daczkowski <trazdacz1@gmail.com>
2016-12-22 17:22:18 -0500

    fixed rotation issue and single fpp issue

--- creategraph.m
+++ creategraph.m
@@ -2,31 +2,37 @@ function creategraph(file,ar,fpp,mode,thresh)
 video=VideoReader(file);
 i=1;
 xdim=floor(video.Duration*video.FrameRate/fpp);
-output=zeros(xdim,1,3);
+output=zeros(1,i,3);
 inputbuffer=zeros(video.Width,video.Height,3,fpp);
-
-while(i<=xdim)
+assert(fpp>0)
+h=waitbar(0,'processing');
+for i=1:xdim
     waitbar(i/xdim);
-    
-    for x=1:fpp
-        inputbuffer(:,:,:,x)=readFrame(video);
+    if fpp>1
+        for x=1:fpp
+            inputbuffer(:,:,:,x)=readFrame(video);
+        end
+        coloronlastdim=shiftdim(inputbuffer,-1);
+    else
+        coloronlastdim=readFrame(video);
     end
-    coloronlastdim=shiftdim(inputbuffer,-1);
+    
     colorvec=reshape(coloronlastdim,[],3);
     colorvec(sum(colorvec)<=thresh,:)=[];
         switch mode
             case 0 %mean
-                output(i,1,:)=mean(colorvec,1);
+                output(1,i,:)=mean(colorvec,1);
                 
             case 1 %median
-                output(i,1,:)=median(colorvec,1);
+                output(1,i,:)=median(colorvec,1);
             case 2 %mode
-                output(i,1,:)=mode(colorvec,1);
+                output(1,i,:)=mode(colorvec,1);
         end
     
     
 end
 output=output/255;
-output=repmat(output,1,round(xdim/ar),1);
+output=repmat(output,1,round(xdim/ar),2);
 figure
 imshow(output)
+imwrite(output,[video.Name,'.png'])
