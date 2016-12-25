7520709abcb0d1f0fa093c87258078f9d2d480c8
Traz Daczkowski <trazdacz1@gmail.com>
2016-12-24 22:31:36 -0500

    fixed rounding bug, added corrected blending

--- creategraph.m
+++ creategraph.m
@@ -1,21 +1,23 @@
 function creategraph(file,ar,fpp,mode,thresh)
 video=VideoReader(file);
 i=1;
-xdim=double(video.Duration*video.FrameRate/fpp);
+xdim=floor(double(video.Duration*video.FrameRate/fpp));
 output=zeros(1,i,3);
 %inputbuffer=zeros(video.Height,video.Width,3,fpp);
 assert(fpp>0)
 h=waitbar(0,'processing');
-for i=1:xdim
+for i=1:xdim-1
     waitbar(i/xdim);
     if fpp>1
         inputbuffer=zeros(video.Height,video.Width,3,fpp);
+        
         for x=1:fpp
             inputbuffer(:,:,:,x)=readFrame(video);
         end
+
         coloronlastdim=shiftdim(inputbuffer,-1);
     else
-        coloronlastdim=readFrame(video);
+        coloronlastdim=double(readFrame(video));
     end
     
 
@@ -24,8 +26,7 @@ for i=1:xdim
     colorvec(sum(colorvec)<=thresh,:)=[];
         switch mode
             case 0 %mean
-                output(1,i,:)=mean(colorvec,1);
-               
+                output(1,i,:)=mean(colorvec.^2,1).^.5;
             case 1 %median
                 output(1,i,:)=median(colorvec,1);
             case 2 %mode
